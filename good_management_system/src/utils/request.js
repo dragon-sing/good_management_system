3b33647a5528f6957dbaeb55a6e02d075b3bb000
jiang <247140155@qq.com>
2021-03-08 11:42:52 +0800

    update:

--- good_management_system/src/utils/request.js
+++ good_management_system/src/utils/request.js
@@ -37,7 +37,7 @@ service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
@@ -58,11 +58,15 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
+        MessageBox.confirm(
+          'You have been logged out, you can cancel to stay on this page, or log in again',
+          'Confirm logout',
+          {
+            confirmButtonText: 'Re-Login',
+            cancelButtonText: 'Cancel',
+            type: 'warning'
+          }
+        ).then(() => {
           store.dispatch('user/resetToken').then(() => {
             location.reload()
           })
@@ -118,15 +122,18 @@ export default function(
 
   option.headers = Object.assign(defaultHeaders, option.headers || {})
 
-  if (mock && process.env.VUE_APP_MOCK_API) { option.url = `${process.env.VUE_APP_MOCK_API}${option.url}` }
+  if (mock && process.env.VUE_APP_MOCK_API) {
+    option.url = `${process.env.VUE_APP_MOCK_API}${option.url}`
+  }
 
   return new Promise((resolve, reject) => {
     service(option)
       .then(resolve)
       .catch(reject)
       .finally(() => {
-        if (loading) { loadingInstance.close() }
+        if (loading) {
+          loadingInstance.close()
+        }
       })
   })
 }
-
